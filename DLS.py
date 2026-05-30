f9543adc9f63f44bc8a4cb0d06c9c835c50ff4ab
ahmedshah <cyberahmedshah@gmail.com>
2026-05-30 23:15:24 +0500

    Made intitial DLS fucntion

--- DLS.py
+++ DLS.py
@@ -1,3 +1,12 @@
 def calculateDLS(target, overs, runs):
+    if overs <= 20:
+        par_score = target * (1 - (overs / 20))
+    else:
+        par_score = target * (1 - (overs / 50))
+    
+    if runs >= par_score:
+        return "Team batting second is ahead"
+    else:
+        return "Team batting second is behind"
     
     
